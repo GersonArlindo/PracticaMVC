2ea05c26c6bd3a70c21b2a7a1b3187336034a763
GersonArlindo <gerson50039@gmail.com>
2022-08-23 16:43:18 -0600

    Accion de editar y eliminar

--- VGStorePractica/Controllers/CategoriasController.cs
+++ VGStorePractica/Controllers/CategoriasController.cs
@@ -39,5 +39,59 @@ namespace VGStorePractica.Controllers
             }
             return View(obj);
         }
+
+        //GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Categorias.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Categorias obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categorias.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Categorias.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        //POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? IdCategoria)
+        {
+            var obj = _db.Categorias.Find(IdCategoria);
+            _db.Categorias.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
